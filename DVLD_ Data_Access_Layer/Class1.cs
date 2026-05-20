35cdea7f548581a30bdd3277eeacb920ca497a4e
aimad <moustajil.dev@gmail.com>
2026-05-20 03:57:34 +0100

    Fix .vs permission issue and clean repo

---
+++ DVLD_ Data_Access_Layer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD__Data_Access_Layer
+{
+    public class Class1
+    {
+    }
+}
